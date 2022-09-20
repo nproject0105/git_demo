01415c444495396431f88e5275d6ce8b96f33efd
nproject0105 <naoking0105@hotmail.com>
2022-09-21 09:39:42 +1000

    [Family Address was Added]
    Requested by Roger

--- git_demo_file.sql
+++ git_demo_file.sql
@@ -1,10 +1,11 @@
 use nwdemo
 
 select 
-[employee ID], [name], [date], [amount], [address], [phone], [email], [fax], [family_member]
+[employee ID], [name], [date], [amount], [address], [phone], [email], [fax], [family_member], [Family Address]
 into #temp
 from tblEmployee t1
 left join tblEmployeeFamily t2
 on t1.name = t2.name
 and t1.date = t2.date
+
 
