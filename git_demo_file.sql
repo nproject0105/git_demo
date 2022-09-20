4363fc2e762038e9339d2981f218701292f3e767
nproject0105 <naoking0105@hotmail.com>
2022-09-20 22:08:07 +1000

    Adding tblEmployeefamily as t2
    Joining t1 and t2 using t1.name = t2.name

--- git_demo_file.sql
+++ git_demo_file.sql
@@ -2,4 +2,6 @@ use nwdemo
 
 select 
 [employee ID], [name], [date], [amount], [address], [phone], [email], [fax]
-from tblEmployee
+from tblEmployee t1
+left join tblEmployeeFamily t2
+on t1.name = t2.name
